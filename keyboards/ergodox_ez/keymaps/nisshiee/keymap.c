cd16d2652ffe211fc95ddef755f026d8ff1ca219
Hirokazu Nishioka <hiro@nisshiee.org>
2019-09-26 20:07:15 +0900

    add windows layer

--- keyboards/ergodox_ez/keymaps/nisshiee/keymap.c
+++ keyboards/ergodox_ez/keymaps/nisshiee/keymap.c
@@ -7,8 +7,9 @@
 #include "keymap_jp.h"
 
 #define BASE 0 // default layer
-#define SYMB 1 // symbols
-#define MDIA 2 // media keys
+#define WIND 1 // for windows
+#define SYMB 2 // symbols
+#define MDIA 3 // media keys
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
@@ -22,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| Space|           | Enter|------+-------+------+------+------+------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M   |   ,  |   .  |   /  |   _  |
  * `--------+------+------+------+------+-------------'           `-------------+-------+------+------+------+------'
- *   |  BS  |  Del |AltShf|LShift| 英数 |                                       |かな/L2|   [  |   ]  |   ^  | ¥   |
+ *   |  BS  |  Del |AltShf|LShift|英数/L1|                                       |かな/L2|   [  |   ]  |   ^  | ¥   |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,---------------.
  *                                        | LSft | Tab  |       | Left | Right  |
@@ -54,7 +55,50 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_UP,
              KC_DOWN,KC_LSFT,LCMD_T(KC_ENT)
     ),
-/* Keymap 1: Symbol Layer
+/* Keymap 1: for Windows layer
+ *
+ * ,--------------------------------------------------.           ,-------------------------------------------------.
+ * | Esc    |   1  |   2  |   3  |   4  |   5  | LEFT |           | RIGHT|   6  |   7   |   8  |   9  |   0  |   -  |
+ * |--------+------+------+------+------+-------------|           |------+------+-------+------+------+------+------|
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  | Opt+Z|           |Sft+En|   Y  |   U   |   I  |   O  |   P  |   @  |
+ * |--------+------+------+------+------+------|      |           |      |------+-------+------+------+------+------|
+ * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J   |   K  |   L  |   ;  |   :  |
+ * |--------+------+------+------+------+------| Space|           | Enter|------+-------+------+------+------+------|
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M   |   ,  |   .  |   /  |   _  |
+ * `--------+------+------+------+------+-------------'           `-------------+-------+------+------+------+------'
+ *   |  BS  |  Del |AltShf|LShift|英数/L1|                                       |かな/L2|   [  |   ]  |   ^  | ¥   |
+ *   `----------------------------------'                                       `----------------------------------'
+ *                                        ,-------------.       ,---------------.
+ *                                        | LSft | Tab  |       | Left | Right  |
+ *                                 ,------|------|------|       |------+--------+------.
+ *                                 |      |      | Home |       |  Up  |        |      |
+ *                                 |Space/|Option|------|       |------| LShift |Enter/|
+ *                                 |Ctrl  |      | Cmd  |       | Down |        |Cmd   |
+ *                                 `--------------------'       `----------------------'
+ */
+// If it accepts an argument (i.e, is a function), it doesn't need KC_.
+// Otherwise, it needs KC_*
+[WIND] = LAYOUT_ergodox(  // layer 1 : windows
+        // left hand
+        KC_ESC,    KC_1,    KC_2,   KC_3,   KC_4,     KC_5,   KC_LEFT,
+        KC_TAB,    KC_Q,    KC_W,   KC_E,   KC_R,     KC_T,   LALT(KC_Z),
+        KC_LCTRL,  KC_A,    KC_S,   KC_D,   KC_F,     KC_G,
+        KC_LSFT,   KC_Z,    KC_X,   KC_C,   KC_V,     KC_B,   KC_SPC,
+        KC_BSPACE, KC_DEL,  LALT(KC_LSFT),  KC_LSHIFT,LT(SYMB, JP_MHEN),
+                                                      KC_LSHIFT,  KC_TAB,
+                                                                  KC_HOME,
+                                            CTL_T(KC_SPC),KC_LWIN,KC_LALT,
+        // right hand
+        KC_RGHT,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
+        LSFT(KC_ENT),       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             JP_AT,
+        KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,           JP_COLN,
+        KC_ENTER,KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,   JP_UNDS,
+        LT(MDIA, JP_HENK),  JP_LBRC,JP_RBRC,JP_CIRC,JP_YEN,
+KC_LEFT,        KC_RIGHT,
+KC_UP,
+KC_DOWN,KC_LSFT,ALT_T(KC_ENT)
+),
+/* Keymap 2: Symbol Layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
@@ -96,7 +140,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS
 ),
-/* Keymap 2: Media and mouse keys
+/* Keymap 3: Media and mouse keys
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |        |      |      |      |      |      |      |           |      |      |      |      |      |      | Reset  |
@@ -129,7 +173,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                   KC_TRNS, KC_TRNS, KC_TRNS,
     // right hand
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET,
-       KC_TRNS,  KC_TRNS, KC_WH_L, KC_WH_U, KC_WH_R, KC_TRNS, KC_TRNS,
+       KC_TRNS,  KC_TRNS, KC_WH_L, KC_WH_U, KC_WH_R, KC_TRNS, TG(WIND),
                  KC_TRNS, KC_BTN1, KC_WH_D, KC_BTN2, KC_UP,   KC_TRNS,
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_BTN3, KC_LEFT, KC_DOWN, KC_RIGHT,
                           KC_VOLU, KC_VOLD, KC_MUTE, KC_TRNS, KC_TRNS,
@@ -155,6 +199,9 @@ void matrix_scan_user(void) {
   ergodox_right_led_3_off();
   switch (layer) {
     // TODO: Make this relevant to the ErgoDox EZ.
+    case WIND:
+      ergodox_right_led_3_on();
+      break;
     case SYMB:
       ergodox_right_led_1_on();
       break;
